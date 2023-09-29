02189713d110efd35d0696a3b8b3e94df88ceec9
Min <70638717+sou05091@users.noreply.github.com>
2023-09-29 11:57:56 +0900

    9/29

--- gSolution/gPrj/gPrjDlg.cpp
+++ gSolution/gPrj/gPrjDlg.cpp
@@ -140,37 +140,7 @@ void CgPrjDlg::OnSysCommand(UINT nID, LPARAM lParam)
 	}
 }
 
-// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
-//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
-//  프레임워크에서 이 작업을 자동으로 수행합니다.
 
-//void CgPrjDlg::OnPaint()
-//{
-//	if (IsIconic())
-//	{
-//		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
-//
-//		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
-//
-//		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
-//		int cxIcon = GetSystemMetrics(SM_CXICON);
-//		int cyIcon = GetSystemMetrics(SM_CYICON);
-//		CRect rect;
-//		GetClientRect(&rect);
-//		int x = (rect.Width() - cxIcon + 1) / 2;
-//		int y = (rect.Height() - cyIcon + 1) / 2;
-//
-//		// 아이콘을 그립니다.
-//		dc.DrawIcon(x, y, m_hIcon);
-//	}
-//	else
-//	{
-//		CDialogEx::OnPaint();
-//	}
-//}
-
-// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
-//  이 함수를 호출합니다.
 HCURSOR CgPrjDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
